572200f411c07685aedefe5640ffe6f8bfe4547f
“juliavbarros” <juliavalenteb@gmail.com>
2025-09-17 12:07:12 +0100

    Corrigindo o erro que eu mesmo criei

--- services/transferenciaservice.js
+++ services/transferenciaservice.js
@@ -6,7 +6,7 @@ const transferencias = [];
 function transferir(remetente, destinatario, valor) {
   const users = getAllUsers();
   if (!remetente || !destinatario || !valor) return { error: 'Dados obrigatórios.' };
-  if (!users.find(u => u.username === remetente)) return { error: 'Remetente, não existe.' };
+  if (!users.find(u => u.username === remetente)) return { error: 'Remetente não existe.' };
   if (!users.find(u => u.username === destinatario)) return { error: 'Destinatário não existe.' };
   if (!isFavorecido(remetente, destinatario) && valor >= 5000) {
     return { error: 'Transferências acima de R$ 5.000,00 só podem ser feitas para favorecidos.' };
